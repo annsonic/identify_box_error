38aefad484467eef5eb442f0a1ea780958fe5062
annsonic <s86184@yahoo.com.tw>
2024-05-25 09:12:30 +0800

    feat: [1] optimize the class Statistician; [2] the class PolygonAnnotator adds progress bar and plot_color_legend

--- app/task/study_in_statistics.py
+++ app/task/study_in_statistics.py
@@ -1,21 +1,23 @@
 import csv
+import os
 from pathlib import Path
 from typing import Union
 
-import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 from app.task.classify_error_type import (TRUE_POSITIVE, DUPLICATE, WRONG_CLASS, BAD_LOCATION,
                                           WRONG_CLASS_LOCATION, BACKGROUND, MISSING)
-from app.task.plot import pie_chart, histogram, line_chart, bar_chart, annotate_polygon
+from app.task.plot import pie_chart, histogram, line_chart, bar_chart, annotate_polygon, plot_color_legend
 from app.utils.metrics import average_precision
 from app.utils.ops import xywh2xyxyxyxy, denormalize_segment
 from app.utils.parse import load_one_label_file
 
 
 error_type_mapper = {
-    'true_positive': TRUE_POSITIVE,
+    # 'true_positive': TRUE_POSITIVE,
     'duplicate': DUPLICATE,
     'wrong_class': WRONG_CLASS,
     'bad_location': BAD_LOCATION,
@@ -31,6 +33,7 @@ class Recorder:
         self.fieldnames = ['image_file_name', 'index', 'object_class', 'error_type', 'confidence']
     
     def write_csv(self, csv_path: Union[str, Path], analysis_results: list[dict]):
+        Path(csv_path).parent.mkdir(parents=True, exist_ok=True)
         with open(csv_path, 'w', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
             writer.writeheader()
@@ -44,43 +47,49 @@ recorder = Recorder()
 
 
 class Statistician:
-    def __init__(self, output_folder_path: Path):
+    def __init__(self, output_folder_path: Path, df: pd.DataFrame):
+        """
+        Args:
+            output_folder_path (Path): The folder path to save the output images.
+            df (pd.DataFrame): The data to be analyzed.
+        """
         self.output_folder_path = output_folder_path
+        self.df_with_positive = df
+        self.df = df[df['error_type'] != 'true_positive']
         self.fp_names = ['background', 'bad_location', 'duplicate', 'wrong_class', 'wrong_class_location']
     
-    def error_proportion(self, source: pd.DataFrame, target_classes: list[int] = None) -> pd.Series:
+    def error_proportion(self, target_classes: list[int] = None) -> tuple[pd.Series, str]:
         """ Calculate the proportion of each error type and draw a pie chart.
         The pie chart will be saved in the self.output_folder_path folder.
         Args:
-            source (pd.DataFrame): The data to be analyzed.
             target_classes (list[int]): The id of classes to be analyzed.
         Returns:
             pd.Series: The proportion of each error type.
+            str: The file name of the pie chart.
         """
-        data = source.copy()
+        data = self.df.copy()
 
-        type_names = list(error_type_mapper.keys())
-        colors = [x.color for x in error_type_mapper.values()]
-        colors = [(x[0] / 255.0, x[1] / 255.0, x[2] / 255.0) for x in colors]
-        hatches = ['/' if i % 2 == 0 else '' for i in range(len(type_names))]
         file_name = 'error_type_pie.png'
         if target_classes:
             data = data[data['object_class'].isin(target_classes)]
             file_name = f"class({'_'.join(map(str, target_classes))})_{file_name}"
 
         fractions = data['error_type'].value_counts(normalize=True)
+        type_names = list(fractions.index)
+        colors = [error_type_mapper[x].color for x in type_names]
+        colors = [(x[0] / 255.0, x[1] / 255.0, x[2] / 255.0) for x in colors]
+        hatches = ['/' if i % 2 == 0 else '' for i in range(len(type_names))]
         pie_chart(fractions, colors, hatches, type_names, self.output_folder_path / file_name)
-        return fractions
+        return fractions, file_name
 
-    def sort_by_errors_per_image(self, source: pd.DataFrame, target_classes: list[int] = None) -> pd.Series:
+    def sort_by_errors_per_image(self, target_classes: list[int] = None) -> pd.Series:
         """ Count the number of errors per image and sort the result in descending order.
         Args:
-            source (pd.DataFrame): The data to be analyzed.
             target_classes (list[int]): The id of classes to be analyzed.
         Returns:
             pd.Series: The number of errors per image.
         """
-        data = source.copy()
+        data = self.df.copy()
         file_name = 'error_type_histogram.png'
         if target_classes:
             data = data[data['object_class'].isin(target_classes)]
@@ -128,14 +137,13 @@ class Statistician:
         
         return result
     
-    def delta_map(self, source: pd.DataFrame, target_classes: list[int] = None, num_classes: int = None
+    def delta_map(self, target_classes: list[int] = None, num_classes: int = None
                   ) -> dict[str, float]:
         """ What mAP would have been if there is no this kind of error. 
         * delta_map is defined as the (metric_after_fixing - metric_before_fixing).
         * Note that all error impacts and the metric will not add to 1.
 
         Args:
-            source (pd.DataFrame): The data to be analyzed.
             target_classes (list[int]): The id of classes to be analyzed.
             num_classes (int): The total number of classes in the original dataset.
                                We need `num_classes` when the `target_classes` is None.
@@ -144,10 +152,10 @@ class Statistician:
         """
         file_name = 'delta_map.png'
         if target_classes:
-            data = source[source['object_class'].isin(target_classes)]
+            data = self.df_with_positive[self.df_with_positive['object_class'].isin(target_classes)]
             file_name = f"class({'_'.join(map(str, target_classes))})_{file_name}"
         else:
-            data = source.copy()
+            data = self.df_with_positive.copy()
             target_classes = range(num_classes)
 
         maps = dict()
@@ -168,20 +176,23 @@ class Statistician:
 
 
 class PolygonAnnotator:
-    def __init__(self, src_img_folder: str, label_folder: str, prediction_folder: str, dst_img_folder: str,
+    def __init__(self, src_img_folder: str, prediction_folder: str, dst_img_folder: str, class_names: dict[int, str],
                  analyzed: pd.DataFrame):
+        # Replace # /images/, /labels/ substrings
+        pattern_image, pattern_labels = f"{os.sep}images{os.sep}", f"{os.sep}labels{os.sep}"
         self.src_img_folder = Path(src_img_folder)
-        self.label_folder = Path(label_folder)
+        self.label_folder = Path(src_img_folder.replace(pattern_image, pattern_labels))
         self.prediction_folder = Path(prediction_folder)
         self.dst_img_folder = Path(dst_img_folder)
         self.dst_img_folder.mkdir(parents=True, exist_ok=True)
+        self.class_names = class_names
         self.analyzed = analyzed
 
     def process_boxes(self, boxes: np.array, size: tuple[int, int]) -> np.array:
         """ Get the box corner points and convert the value to the original resolution. 
         Args:
             boxes (np.array): regular box dim=(n, 4); rotated box dim=(n, 8)
-            size tuple[int, int]: the image (height, width)
+            size (tuple[int, int]): the image (height, width)
         Returns:
             boxes (np.array): dim=(n, 4, 2)
         """
@@ -195,14 +206,16 @@ class PolygonAnnotator:
         return boxes
         
     def run(self):
+        plot_color_legend([error_type_mapper[x].color for x in error_type_mapper],
+                          list(error_type_mapper), str(self.dst_img_folder / 'color_legend.png'))
         grouped = self.analyzed.groupby('image_file_name')
 
-        for image_file_name, df in grouped:
+        for image_file_name, df in tqdm(grouped, total=len(grouped), desc='Annotating Images'):
             txt_file_name = Path(str(image_file_name)).stem + '.txt'
             gt_classes, gt_boxes = load_one_label_file(str(self.label_folder / txt_file_name), has_conf=False)
             pd_classes, pd_boxes, _ = load_one_label_file(str(self.prediction_folder / txt_file_name), has_conf=True)
-            img = cv2.imread(str(self.src_img_folder / str(image_file_name)))
-            size = (img.shape[0], img.shape[1])
+            img = plt.imread(str(self.src_img_folder / str(image_file_name)))
+            size = (img.shape[1], img.shape[0])
             gt_boxes = self.process_boxes(gt_boxes, size)
             pd_boxes = self.process_boxes(pd_boxes, size)
             annotations = []
@@ -210,8 +223,13 @@ class PolygonAnnotator:
                 if row['error_type'] == 'missing':  # Read the ground truth label
                     polygon = gt_boxes[row['index']]
                     color = error_type_mapper['missing'].color
-                else:  # Read the prediction label
+                    name = self.class_names[gt_classes[row['index']]]
+                elif row['error_type'] != 'true_positive':  # Read the prediction label
                     polygon = pd_boxes[row['index']]
                     color = error_type_mapper[row['error_type']].color
-                annotations.append((polygon, color))
-            annotate_polygon(img, annotations, str(self.dst_img_folder / image_file_name))
+                    name = self.class_names[row['object_class']]
+                else:
+                    continue
+                annotations.append((polygon, color, name))
+            if annotations:
+                annotate_polygon(img, annotations, str(self.dst_img_folder / image_file_name))
